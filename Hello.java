f71894ce6700629400015b4b9d9a626fff751de2
Roop Kumar Chatterjee <chatterjee.roopkumar17@gmail.com>
2021-08-27 12:58:03 +0530

    Printing Hello World in java using print and println

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("Hello");
+        System.out.print("World\n");
+        //By default print will print Hello and world in same line
+        System.out.println("Hello");
+        System.out.println("World");
+        //By default println will print Hello and world in different lines
+
+    }
+    
+}
